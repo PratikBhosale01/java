72ecb16bf9cd71dd35b2fee41b71a1bf97bad5ad
Pratik Bhosale <pratikbhosale0003@gmail.com>
2024-02-22 23:20:59 +0530

    basic

---
+++ Conditonals/Basic.java
@@ -0,0 +1,21 @@
+/*
+ * Author : Pratik Bhosale
+ * Date : $(DATE)
+ * created with : IntelliJ IDEA Community Edition
+ */
+
+
+package Conditonals;
+
+public class Basic {
+
+        public static void main(String args[]) {
+            double a, b, c;
+            a = 3.0/ 0;
+            b = 0 / 4.0;
+            c = 0 / 0.0;
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(c);
+        }
+}
